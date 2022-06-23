d678a8fca0f7796991176c18181aa5eb61bcafd5
brito101 <rodrigobrito101@hotmail.com>
2022-06-22 21:40:26 -0300

    ADD MongoDB connection and model user

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,24 +1,36 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express"
 
-import { Product } from '../models/Product';
+import { Product } from "../models/Product"
+import User from "../models/User"
 
-export const home = (req: Request, res: Response)=>{
-    let age: number = 90;
-    let showOld: boolean = false;
+export const home = async (req: Request, res: Response) => {
+  let users = await User.find({}).sort({ email: 1 })
+  //   let users = await User.findOne({
+  //     email: "rodrigobrito101@hotmail.com",
+  //   })
+  //   let users = await User.findById("62b3b28fbd1cb71e956b5f28")
+  //   let users = await User.find({
+  //     "name.firstName": "Rodrigo",
+  //   })
 
-    if(age > 50) {
-        showOld = true;
-    }
+  console.log(users)
 
-    let list = Product.getAll();
-    let expensiveList = Product.getFromPriceAfter(12);
+  let age: number = 90
+  let showOld: boolean = false
 
-    res.render('pages/home', {
-        name: 'Bonieky',
-        lastName: 'Lacerda',
-        showOld,
-        products: list,
-        expensives: expensiveList,
-        frasesDoDia: []
-    });
-};
+  if (age > 50) {
+    showOld = true
+  }
+
+  let list = Product.getAll()
+  let expensiveList = Product.getFromPriceAfter(12)
+
+  res.render("pages/home", {
+    name: "Bonieky",
+    lastName: "Lacerda",
+    showOld,
+    products: list,
+    expensives: expensiveList,
+    frasesDoDia: [],
+  })
+}
